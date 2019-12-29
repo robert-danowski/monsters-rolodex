62cfc5799f6b097cf1dc07ae9bf4b91b1ff34807
Robert Danowski <trevny@poczta.onet.pl>
2019-12-29 20:47:12 +0100

    Updates

---
+++ precache-manifest.86a06a75ec2779d2768ac9d148caf872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a958ada9d202d2e74895f1272d34d8f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "50dbc178e030ab6f7425",
+    "url": "/monsters-rolodex/static/css/main.86fa8a5d.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50dbc178e030ab6f7425",
+    "url": "/monsters-rolodex/static/js/main.fa307e2b.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
